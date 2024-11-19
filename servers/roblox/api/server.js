f254fe4a517c71625798c7bc32235cc0c1ecd680
cbgames-xyz <106137185+EatMYSHOORTS@users.noreply.github.com>
2024-11-19 20:25:48 +1300

    roadblocked

--- servers/roblox/api/server.js
+++
@@ -1,7 +0,0 @@
-const fetch = require("node-fetch");
-
-module.exports = async (req, res) => {
-    const response = await fetch("http://worldtimeapi.org/api/timezone/Pacific/Auckland");
-    const data = await response.json();
-    res.status(200).json(data);
-};
