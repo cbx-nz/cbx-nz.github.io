13b9aa3afbed11ef6226f7d63fff53804194659e
cbgames-xyz <106137185+EatMYSHOORTS@users.noreply.github.com>
2024-11-22 11:21:10 +1300

    //

--- updatedtooltip.js
+++ updatedtooltip.js
@@ -1,4 +1,4 @@
-fetch('./updatedtooltip.html')
+fetch('https://cbgames-xyz.vercel.app/updatedtooltip.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('content-placeholder').innerHTML = data;
