b8097de8d1751ce35d4a4cb0404e7a2a48a4793d
cbgames-xyz <christmastvonlineclothes@gmail.com>
2025-04-02 20:03:19 +1300

    somehow this works?

--- reviews/assets/js/firebase.js
+++ reviews/assets/js/firebase.js
@@ -1,13 +1,23 @@
 // /reviews/assets/js/firebase.js or ./assets/js/firebase.js
 // firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyChZrNkf29uagia4QBq5CwkpU5nCP-D390",
+  authDomain: "cbx-reviews.firebaseapp.com",
+  projectId: "cbx-reviews",
+  storageBucket: "cbx-reviews.firebasestorage.app",
+  messagingSenderId: "839457911591",
+  appId: "1:839457911591:web:d386e925c380dadc782f02",
+  measurementId: "G-PFJMBG503D"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
