91ce4ac0602c8032f900231461ca7031de4066c3
cbgames-xyz <christmastvonlineclothes@gmail.com>
2025-04-02 20:21:33 +1300

    firebase.firestore import

--- reviews/assets/js/firebase.js
+++ reviews/assets/js/firebase.js
@@ -3,6 +3,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore();
 const analytics = getAnalytics(app);
+
+export { app, db, analytics };
